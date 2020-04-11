5b20e3e700286aa3b5d314c2ac34d0fae0347c88
Egor Batrakov <goshanprm@gmail.com>
2020-04-11 15:51:50 +0300

    Complete 3 exerceses

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  const base = possible.length;
+  let result = '';
+  for (let i = 0; i < length; i += 1) {
+    const index = Math.floor(Math.random() * base);
+    result += possible[index];
+  }
+  return result;
 };
 
 module.exports = { generateKey };
--- Exercises/3-ip.js
+++ Exercises/3-ip.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const ipToInt = (ip = '127.0.0.1') => {
-  // Parse ip address as string, for example '10.0.0.1'
-  // to ['10', '0', '0', '1'] to [10, 0, 0, 1]
-  // and convert to Number value 167772161 with sitwise shift
-  // (10 << 8 << 8 << 8) + (0 << 8 << 8) + (0 << 8) + 1 === 167772161
-  // Use Array.prototype.reduce of for loop
+  const ipArray = ip.split('.');
+  const arrayWithNumbers = ipArray.map(item => +item);
+  arrayWithNumbers[0] = arrayWithNumbers[0] << 8 << 8 << 8;
+  arrayWithNumbers[1] = arrayWithNumbers[1] << 8 << 8;
+  arrayWithNumbers[2] <<= 8;
+  const sumOfElements = arrayWithNumbers.reduce((sum, current) => sum + current, 0);
+  return sumOfElements;
 };
 
 module.exports = { ipToInt };
